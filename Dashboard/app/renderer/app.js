152efeeb9961bd292960904b783b0bc7352d07e3
Ghost <31184695+GHOSCHT@users.noreply.github.com>
2020-08-01 16:29:29 +0200

    Add darkmode

--- Dashboard/app/renderer/app.js
+++ Dashboard/app/renderer/app.js
@@ -4,17 +4,36 @@ import Test from './components/Test';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './store/reducers';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import { ThemeProvider } from '@material-ui/core/styles';
+import LampButtonList from './components/LampButtonList';
 
 const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
+const darkTheme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#23272A',
+    },
+    secondary: {
+      main: '#7289DA',
+    },
+  },
+});
+
 const rootElement = document.querySelector(document.currentScript.getAttribute('data-container'));
 
 ReactDOM.render(
   <Provider store={store}>
-    <Test />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <LampButtonList />
+    </ThemeProvider>
   </Provider>,
   rootElement,
 );
