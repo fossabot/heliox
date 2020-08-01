10fa1c91d5633025278157b6816763b01a1bf0b3
Ghost <31184695+GHOSCHT@users.noreply.github.com>
2020-08-01 15:42:10 +0200

    Add redux store

---
+++ Dashboard/app/renderer/store/reducers/comConnectionReducer.js
@@ -0,0 +1,14 @@
+import SerialPort from 'serialport';
+const Readline = require('@serialport/parser-readline');
+
+const port = new SerialPort('COM3', {
+  baudRate: 9600,
+});
+
+const parser = port.pipe(new Readline({ delimiter: '\r\n' }));
+
+const initialState = { port, parser };
+
+export default (state = initialState, { type, payload }) => {
+  return state;
+};
---
+++ Dashboard/app/renderer/store/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import comConnectionReducer from './comConnectionReducer';
+
+const rootReducer = combineReducers({ comConnectionReducer });
+
+export default rootReducer;
