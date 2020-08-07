59b6b5ad275af65a2388b3a112cd48894f691e57
Ghost <31184695+GHOSCHT@users.noreply.github.com>
2020-08-07 16:04:01 +0200

    Hardware config changes

--- Arduino/ArduinoRelayControl.ino
+++ Arduino/ArduinoRelayControl.ino
@@ -2,9 +2,9 @@
 
 const int relayCount = 4;
 
-const int relayPin[relayCount] = {3, 4, 5, 6};
-const int btnPin[relayCount] = {7, 8, 9, 10};
-byte relayState[relayCount] = {HIGH, HIGH, HIGH, HIGH}; //high -> relay off
+const int relayPin[relayCount] ={ 2, 3, 5, 6 };
+const int btnPin[relayCount] ={ 7, 8, 9, 10 };
+byte relayState[relayCount] ={ HIGH, HIGH, HIGH, HIGH }; //high -> relay off
 
 byte buttonState[relayCount];
 byte lastButtonState[relayCount];
--- Dashboard/app/renderer/store/reducers/comConnectionReducer.js
+++ Dashboard/app/renderer/store/reducers/comConnectionReducer.js
@@ -1,7 +1,7 @@
 import SerialPort from 'serialport';
 const Readline = require('@serialport/parser-readline');
 
-const port = new SerialPort('COM3', {
+const port = new SerialPort('COM6', {
   baudRate: 9600,
 });
 
