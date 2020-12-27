3ad95d8863a560a81114c0005f53056349df911a
Ghost <31184695+GHOSCHT@users.noreply.github.com>
2020-12-27 16:55:38 +0100

    Start Console firmware

---
+++ Arduino/Console/src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+
+void setup()
+
+} // put your setup code here, to run once:
+
+vo o d l o d l o(d l od ldv
+                     i
+
+                         o
+                             p
