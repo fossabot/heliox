e89d12f13b76dd0ea31b0f1f2d554f0846f41604
GHOSCHT <31184695+GHOSCHT@users.noreply.github.com>
2021-07-10 16:19:37 +0200

    Use styled-components for window border

--- Dashboard/src/App.tsx
+++ Dashboard/src/App.tsx
@@ -1,32 +1,40 @@
 import React from "react";
-// import SerialPort from "serialport";
-import { remote } from "electron";
+import SerialPort from "serialport";
+import { createGlobalStyle } from "styled-components";
 import Knob from "./Components/Knob";
 
-const App = () => {
-  // https://www.electronjs.org/docs/api/system-preferences#systempreferencesgetcolorcolor-windows-macos
-  console.log(remote.systemPreferences.getColor("active-border")); // border
-  // https://www.electronjs.org/docs/api/system-preferences#systempreferencesgetaccentcolor-windows-macos
-  console.log(remote.systemPreferences.getAccentColor()); // accent color
+const GlobalStyle = createGlobalStyle`
+  html {
+    border-style: solid;
+    border-color: #363636;
+    border-width: 1px;
+    box-sizing: border-box;
+    height: 100%;
+    border-bottom-style: hidden;
+    border-right-style: hidden;
+  }
+`;
 
-  // const port = new SerialPort("COM5", {
-  //   baudRate: 9600,
-  //   autoOpen: false,
-  // });
-  // const handler = () => {
-  //   // eslint-disable-next-line
-  //   port.write("off", (err) => {
-  //     if (err) {
-  //       return console.log("Error on write: ", err.message);
-  //     }
-  //     console.log("message written");
-  //   });
-  // };
+const App = () => {
+  const port = new SerialPort("COM5", {
+    baudRate: 9600,
+    autoOpen: false,
+  });
+  const handler = () => {
+    // eslint-disable-next-line
+    port.write("off", (err) => {
+      if (err) {
+        return console.log("Error on write: ", err.message);
+      }
+      console.log("message written");
+    });
+  };
 
   return (
     <div>
+      <GlobalStyle />
       <Knob increase={() => { console.log("inc"); }} decrease={() => { console.log("dec"); }} toggle={() => { console.log("togg"); }} />
-      {/* <button
+      <button
         type="button"
         onClick={() => {
           port.open();
@@ -55,9 +63,8 @@ const App = () => {
         }}
       >
         list
-      </button> */}
+      </button>
     </div>
   );
 };
-
 export default App;
