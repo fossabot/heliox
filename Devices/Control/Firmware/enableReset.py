2a5c3e8e13563ea8919c09e2a04cd181117f51cc
GHOSCHT <31184695+GHOSCHT@users.noreply.github.com>
2021-08-24 21:44:51 +0200

    Serial Port is now provided as argument

--- Devices/Control/Firmware/enableReset.py
+++ Devices/Control/Firmware/enableReset.py
@@ -1,6 +1,24 @@
 import serial
 import time
-ser = serial.Serial("COM5")
-ser.write(b'r')
-ser.close()
-time.sleep(1)
+import sys
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+if len(sys.argv) == 1:
+    print(f"{bcolors.FAIL}Please enter COM Port as argument{bcolors.ENDC}")
+
+else:
+    ser = serial.Serial(sys.argv[1])
+    ser.write(b'r')
+    ser.close()
+    print(f"{bcolors.OKGREEN}Reset enabled{bcolors.ENDC}")
+    time.sleep(1)
