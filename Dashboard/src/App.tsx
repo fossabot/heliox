455211d1a8848f13aa6d523f42a70fce53b03559
GHOSCHT <31184695+GHOSCHT@users.noreply.github.com>
2021-08-27 13:03:23 +0200

    Serial Port can now be set in-app

--- Dashboard/src/App.tsx
+++ Dashboard/src/App.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { createGlobalStyle } from "styled-components";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "typesafe-actions";
+import Select from "react-select";
+import SerialPort from "serialport";
 import Knob from "./Components/Knob";
 import { connect, disconnect } from "./redux/actions/asyncSerialConnectionActions";
 import { setSerialPort } from "./redux/actions/serialConnectionActions";
@@ -18,15 +20,30 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
+const options: [{value: string, label:string}] = [{ value: "placeholder", label: "Select Port" }];
+
+const fetchPorts = async () => {
+  const data = await SerialPort.list();
+  options.shift();
+  data.forEach((comPort) => { options.push({ value: comPort.path, label: comPort.path }); });
+};
+
 const App = () => {
   const [status, setStatus] = useState(0);
   const dispatch = useDispatch();
   const serialConnection = useSelector<RootState, RootState["serialConnection"]>((state) => state.serialConnection);
-
   const SerialDataListener = (data: string) => {
     const parsedData = data.split(",");
     console.log(parsedData);
     setStatus(parseInt(parsedData[2], 10));
+  };
+
+  fetchPorts();
+
+  const [portSelectionValue, setPortSelectionValue] = useState(options[0]);
+  const handlePortChange = async (selectedOption: any) => {
+    setPortSelectionValue(selectedOption);
+    dispatch(setSerialPort(selectedOption.value));
   };
 
   useEffect(() => {
@@ -72,7 +89,6 @@ const App = () => {
       <button
         type="button"
         onClick={() => {
-          dispatch(setSerialPort("COM5"));
           dispatch(connect());
         }}
       >
@@ -86,6 +102,11 @@ const App = () => {
       >
         disconnect
       </button>
+      <Select
+        value={portSelectionValue}
+        onChange={handlePortChange}
+        options={options}
+      />
     </div>
   );
 };
