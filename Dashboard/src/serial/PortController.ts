e9364c73bff66eb93fcdfafcda88486cc5b11b14
GHOSCHT <31184695+GHOSCHT@users.noreply.github.com>
2021-12-31 22:18:35 +0100

    Simplify class usage

--- Dashboard/src/serial/PortController.ts
+++ Dashboard/src/serial/PortController.ts
@@ -1,44 +1,38 @@
 import SerialPort, { parsers } from "serialport";
 
 class PortController {
-  path: null | string;
+  path: string;
 
-  port: null | SerialPort;
+  port: SerialPort;
 
   parser: any;
 
   onCloseCallback: (error:Error)=>void;
 
-  constructor() {
-    this.path = null;
-    this.port = null;
-    this.onCloseCallback = () => {};
-  }
-
-  get isOpen() {
-    if (!this.port) {
-      return false;
-    }
-    return this.port.isOpen;
-  }
-
-  create(path: string) {
+  constructor(path: string) {
     this.path = path;
     this.port = new SerialPort(path, {
       baudRate: 9600,
       autoOpen: false,
     });
-
     this.parser = this.port.pipe(new parsers.Readline({ delimiter: "\r\n" }));
+    this.onCloseCallback = () => {};
+  }
+
+  get isOpen(): boolean {
+    if (this.port == null) {
+      return false;
+    }
+    return this.port.isOpen;
+  }
+
+  get getParser(): any {
+    return this.parser;
   }
 
   open(callback: (error:Error | null | undefined)=>void, onCloseCallback:(error:Error | null | undefined)=>void) {
     if (this.isOpen) {
       throw new Error("Port already open");
-    }
-
-    if (this.port === null) {
-      throw new Error("Port must be created first");
     }
 
     this.port.open((error) => callback(error));
